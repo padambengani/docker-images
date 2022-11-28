5b015cbc5dc632bf660c084810df432f2ccaafb9
Padam Prakash Bengani <padam.bengani@oracle.com>
2022-11-28 15:30:01 +0000

    Apply shellcheck fixes (INFO/STYLE)

--- OracleAnalytics/dockerfiles/buildDockerImage.sh
+++ OracleAnalytics/dockerfiles/buildDockerImage.sh
@@ -120,12 +120,12 @@ echo "Building image '$IMAGE_NAME' ..."
 
 # BUILD THE IMAGE (replace all environment variables)
 BUILD_START=$(date '+%s')
-docker build --force-rm=$NOCACHE --no-cache=$NOCACHE $SQUASH $PROXY_SETTINGS -t $IMAGE_NAME -f Dockerfile . || {
+docker build "--force-rm=$NOCACHE --no-cache=$NOCACHE $SQUASH $PROXY_SETTINGS -t $IMAGE_NAME -f Dockerfile ." || {
   echo "There was an error building the image."
   exit 1
 }
 BUILD_END=$(date '+%s')
-BUILD_ELAPSED=`expr "$BUILD_END" - "$BUILD_START"`
+BUILD_ELAPSED=$(( BUILD_END - BUILD_START ))
 
 echo ""
 
